18e00c80ee61c3d8ca54242d001018c3015cd9f0
afnabdillah <afnabdillah@gmail.com>
2023-04-05 21:31:31 +0700

    feat: google sign in done

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,6 +2,8 @@ const { checkPassword } = require('../helpers/bcrypt');
 const { createToken } = require('../helpers/jwt');
 const { sendMailWithNodeMailer } = require('../helpers/nodemailer');
 const { Users, Profile } = require('../models');
+const { OAuth2Client } = require('google-auth-library');
+
 
 class UserController {
 
@@ -9,8 +11,8 @@ class UserController {
     try {
       const newUser = await Users.create(req.body);
       await Profile.create({
-        UserId : newUser.id,
-        userPhoto : 'https://randomuser.me/api/portraits/lego/2.jpg'
+        UserId: newUser.id,
+        userPhoto: 'https://randomuser.me/api/portraits/lego/2.jpg'
       });
       sendMailWithNodeMailer(newUser.email);
       res.status(201).json(newUser);
@@ -41,10 +43,10 @@ class UserController {
     try {
       const user = req.user;
       const profile = await Profile.findOne({
-        where : {UserId : user.id}
+        where: { UserId: user.id }
       })
       res.status(200).json(profile);
-    } catch(err) {
+    } catch (err) {
       next(err);
     }
   }
@@ -52,13 +54,44 @@ class UserController {
   static async updateUserProfile(req, res, next) {
     try {
       const user = req.user;
-      await Profile.update({...req.body}, {
-        where : {
-          UserId : user.id
+      await Profile.update({ ...req.body }, {
+        where: {
+          UserId: user.id
         }
       })
-      res.status(200).json({message : 'Your Profile Has Been Updated!'})
-    } catch(err) {
+      res.status(200).json({ message: 'Your Profile Has Been Updated!' })
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async googleSignIn(req, res, next) {
+    try {
+      const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+      const ticket = await client.verifyIdToken({
+        idToken: req.headers.credential,
+        audience: process.env.GOOGLE_CLIENT_ID,
+      });
+      const payload = ticket.getPayload();
+      const { email, picture, name } = payload;
+      const [user, created] = await Users.findOrCreate({
+        where: { email },
+        defaults: {
+          username: email.split('@')[0],
+          email: email,
+          password: 'google',
+        },
+        hooks : false
+      })
+      if (created) {
+        await Profile.create({
+          fullName: name,
+          UserId: user.id,
+          userPhoto: picture
+        })
+      }
+      res.status(200).json(user);
+    } catch (err) {
       next(err);
     }
   }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,7 @@ const authentication = require('../middlewares/authentication');
 
 userRouter.post('/signup', UserController.signup);
 userRouter.post('/login', UserController.login);
+userRouter.post('/google-sign-in', UserController.googleSignIn);
 userRouter.get('/profile', authentication, UserController.getUserProfile);
 userRouter.put('/profile', authentication,  UserController.updateUserProfile);
 
